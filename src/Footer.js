0163dc59634deca1ec95f45883dff7d5c08597a5
TakehiroTsukagoshi <takehiro.thanks@gmail.com>
2023-05-05 15:17:37 +0900

    ヘッダーとフッターのコンポーネントを作成

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>©️ 2020 Airbnb clone</p>
+      <p>Private・Term・Sitemap・Company Details</p>
+    </div>
+  )
+}
+
+export default Footer
